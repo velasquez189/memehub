0aa6b97a0e46b09ea5f9c784b777dd3421359a4b
Andrea Velasquez <velasquez189@gmail.com>
2018-05-31 22:52:22 -0400

    config.js template using .env; schema/seeds.sql updated

---
+++ config/config.js
@@ -0,0 +1,23 @@
+const mysql = require(`mysql`)
+require(`dotenv`).load();
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        password: process.env.DB_PASSWORD,
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE
+    })
+};
+
+connection.connect(err => {
+    if (err) {
+        console.error(`Congratulations, you've played yourself: ${err.stack}`);
+        return;
+    }
+    console.log(`You've connected succesfully: ${connection.threadId}`)
+});
+
+module.exports = connection;
