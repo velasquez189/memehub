b1c4ef52834dc18407d0596ec62dbecab00ed612
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-06-02 11:26:36 -0400

    changed s3 file name

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/test.html"));
+    res.sendFile(path.join(__dirname, "../public/views/S3test.html"));
   });
 
   app.get("/cognito", function(req,res) {
