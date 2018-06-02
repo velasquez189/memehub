b11a0a151ddc5de2e28adbb001279c0b6f426289
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-06-01 23:35:44 -0400

    incorporated AWS... currently have S3 *working*

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/views/test.html"));
   });
 
   app.get("/cognito", function(req,res) {
