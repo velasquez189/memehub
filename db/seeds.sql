d5eeec9dc9f1ca650ff1ba58378bb3bc16ba750e
Andrea Velasquez <velasquez189@gmail.com>
2018-06-01 23:16:01 -0400

    Tables created and joined

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,29 @@
 USE meme_pieDB;
 
-INSERT INTO images (img_id, img_name, img_type, dated)
-VALUES ('id', 'name', 'medium', NOW());
+INSERT INTO memes (file_path)
+VALUES ('memes/andrea.jpeg');
+INSERT INTO memes (file_path)
+VALUES ('memes/gayland.jpeg');
+INSERT INTO memes (file_path)
+VALUES ('memes/rathew.jpeg');
+
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (1, 'student');
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (1, 'columbine');
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (1, 'whamen');
+
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (2, 'student');
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (2, 'murican');
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (2, 'cyka');
+
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (3, 'student');
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (3, 'gaylandian');
+INSERT INTO tagged (meme_id, tag_id)
+VALUES (3, 'soy boi');
